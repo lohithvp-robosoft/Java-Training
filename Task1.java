e7c3dcdf118866048d21fff6a5e43e42109a6899
lohithvp-robosoft <lohith.vp@robosoftin.com>
2024-10-18 15:28:42 +0530

    Removed import

--- Task1.java
+++ Task1.java
@@ -1,8 +1,6 @@
-
-import java.util.Arrays;
-
 public class Task1 {
     public static void main(String[] args) {
+        System.out.println();
         System.out.println("Hello");
         String firstName = "Jeevan";
         String lastName = "Jain";
